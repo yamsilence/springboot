64f03af251bea7876b6601339800162631826a03
lhp <2221213632@qq.com>
2019-11-28 19:45:16 +0800

    练习

--- src/main/java/com/epoint/SpbootmyApplication.java
+++ src/main/java/com/epoint/SpbootmyApplication.java
@@ -3,6 +3,8 @@ package com.epoint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 @MapperScan(value="com.epoint.dao")
 @SpringBootApplication
 public class SpbootmyApplication {
